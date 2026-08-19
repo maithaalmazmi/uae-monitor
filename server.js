52f8a4f9d9f9b542a1bdab5e4aa2a331124f9a97
maithaalmazmi <maithaalmazmi@gmail.com>
2026-08-19 20:33:41 +0400

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ async function cycle() {
   }
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`UAE Monitor running on http://localhost:${PORT}`);
   cycle(); // initial fill
